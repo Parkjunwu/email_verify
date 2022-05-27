0e1363004a241a9706ea2c33783f00e4e57b04cf
Parkjunwu <phak9402@gmail.com>
2022-05-27 18:00:40 +0900

    apollo config change

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,7 @@ module.exports = {
       includes:["./src/**/*.{ts,tsx}"],
       tagName:"gql",
       name:"instaclone-backend",
-      url:process.env.NODE_ENV==="production" ? process.env.REACT_APP_URI : "http://localhost:4000/graphql",
+      url:"http://localhost:4000/graphql",
     }
   }
 }
